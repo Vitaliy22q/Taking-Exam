1722ace217ada30fb505646c3378cb26a59f2d01
Vitaliy22q <Vitaliy22q@list.ru>
2016-06-10 09:23:43 +0300

    Add files via upload

---
+++ Exams2.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int mas[200],n,i;
+	printf("vvedite n=");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+	 mas[i]=2*i+1;
+	 printf("\nmas[%d]=%d",i,mas[i]);	
+	
+	
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
